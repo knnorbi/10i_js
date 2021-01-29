2970519aa6d5fa1f01bd298e9c88bb9b2713ba92
knnorbi <kluka.norbert@gmail.com>
2021-01-29 09:18:17 +0100

    Initial commit

---
+++ 20210129/script.js
@@ -0,0 +1,11 @@
+function helloworld() {
+    // window.alert("Hello World!");
+    // document.getElementById("message").innerHTML = "Hello World!";
+    
+    var a = 5;
+    a = "asd";
+    
+    a = 4 + "6" + 1;
+    
+    window.alert(a);
+}

c79c6d1a376195760e1cdae805ba1d60be3c7184
knnorbi <kluka.norbert@gmail.com>
2021-05-07 12:25:59 +0200

    fix

--- 20210430/script.js
+++ 20210430/script.js
@@ -2,29 +2,20 @@ function validate() {
     document.getElementById("nameLabel").style.color = "black";
     document.getElementById("ageLabel").style.color = "black";
     
-    document.getElementById("nameHint").style.display = "none";
-    document.getElementById("ageHint").style.display = "none";
-    document.getElementById("agreeHint").style.display = "none";
-    
-    var valid = true;
+    var vaild = true;
     
     if(document.getElementById("nameInput").value == "") {
-        document.getElementById("nameHint").style.display = "inline";
+        alert("Name is missing.");
         document.getElementById("nameLabel").style.color = "red";
-        valid = false;
+        vaild = false;
     }
     
     var age = document.getElementById("ageInput").value;
     if(age < 1 || age > 120) {
-        document.getElementById("ageHint").style.display = "inline";
+        alert("Age is wrong.");
         document.getElementById("ageLabel").style.color = "red";
         valid = false;
     }
     
-    if(document.getElementById("agree").checked == false) {
-        document.getElementById("agreeHint").style.display = "inline";
-        valid = false;
-    }
-    
-    return valid;
+    return vaild;
 }
